ad6711f4ec414105540311b5e8eefe02b4f2036a
Ramfi <Ramfiaogusto@gmail.com>
2022-11-14 19:19:28 -0500

    muestra los parrafos sucios

--- inicio.py
+++ inicio.py
@@ -6,19 +6,25 @@ from googlesearch import search
 from bs4 import BeautifulSoup
 
 #------------------------------Libreria google o google search-------
-lista_link=[]
+lista_link=[""]
 
 pregunta_google= str(input("ingresa tu pregunta: "))
 
 for i in search(pregunta_google, num_results=4, lang="es"):
 	lista_link.append(i)
-	
 
-print(lista_link)
+def buscar(num):
+	response = requests.get(lista_link[num])
+	print(response)
+	print(lista_link[num])
+	soup=BeautifulSoup(response.content, 'html.parser')
+	content = response.content  
+	content_html = soup.find_all("p")
+	print(content_html)
 
 
 
-
+buscar(2)
 
 
 #---------------------------------------------------------------------
