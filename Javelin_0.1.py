2a1d3cb1496e8fe9cf255198ad6ee95d3d602f07
Ramfi <Ramfiaogusto@gmail.com>
2022-11-27 12:56:51 -0500

    Fallo arreglado,

--- Javelin_0.1.py
+++ Javelin_0.1.py
@@ -7,24 +7,29 @@ from bs4 import BeautifulSoup
 
 
 #------------------declaracion de funciones--------------------------
-
+"""
 def selec_link(num_link):
-	response = requests.get(links[num_link])
+	response = requests.get(links(num_link))
 	soup=BeautifulSoup(response.content, 'html.parser')
 	parrafos=soup.find_all("p")
 	print("esta respuesta es de la pagina \n\n " + links[num_link])
 	print(parrafos + "\n \n" + "------------------AQUI TERMINAN LOS PARRAFOS--------------------")
 	
 
-
+"""
 #------------------aqui busca y lista 5 links de cada pregunta-------
-links=['uno', 'dos']
+
+
+
+links=[]
 
 pregunta_google= input("ingresa tu pregunta: ")
 for i in search(pregunta_google, num_results=5, lang="es"):
-	
-	links.append=[i]
-print(links)
+	print(i)
+	links.append(i)
+
+#print(links)
+
 
 #selec_link(3)
 #---------------------------------------------------------------------
