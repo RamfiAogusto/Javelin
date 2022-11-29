828a2be05af17d9d33fe1eafbfd31c5eda94f557
Ramfi <Ramfiaogusto@gmail.com>
2022-11-29 16:55:42 -0500

    muestra parrafos sucios

--- Javelin_0.1.py
+++ Javelin_0.1.py
@@ -3,19 +3,20 @@
 import requests
 from googlesearch import search
 from bs4 import BeautifulSoup
+import re
 
 
 
 #------------------declaracion de funciones--------------------------
-"""
+
 def selec_link(num_link):
-	response = requests.get(links(num_link))
+	response = requests.get(links[num_link])
 	soup=BeautifulSoup(response.content, 'html.parser')
 	parrafos=soup.find_all("p")
 	print("esta respuesta es de la pagina \n\n " + links[num_link])
-	print(parrafos + "\n \n" + "------------------AQUI TERMINAN LOS PARRAFOS--------------------")
+	print(parrafos)
 	
-"""
+
 
 #------------------aqui busca y lista 5 links de cada pregunta-------
 
@@ -25,13 +26,13 @@ links=[]
 
 pregunta_google= input("ingresa tu pregunta: ")
 for i in search(pregunta_google, num_results=5, lang="es"):
-	print(i)
+	
 	links.append(i)
 
-print(links)
 
 
-#selec_link(0)
+
+selec_link(0)
 #---------------------------------------------------------------------
 
 
