3596a89a82b2927b1ab655bae1cea701c3206d65
Ramfi <Ramfiaogusto@gmail.com>
2022-12-04 15:42:40 -0500

    Javelin_full_0.1.1

--- Javelin_0.1.py
+++ Javelin_0.1.py
@@ -7,26 +7,25 @@ import re
 
 
 
-#------------------declaracion de funciones--------------------------
+#------------------declaracion de funciones y variables--------------------------
+links=[]
+lista_parrafos=[]
 
 def selec_link(num_link):
 	response = requests.get(links[num_link])
 	soup=BeautifulSoup(response.content, 'html.parser')
 	parrafos=soup.find_all("p")
-	print("esta respuesta es de la pagina \n\n " + links[num_link])
-	print(parrafos)
-	
+	print("esta respuesta es de la pagina: " + links[num_link])
+	for p in range(len(parrafos)):
+		parrafo_limpio = parrafos[p].get_text()
+		lista_parrafos.append(parrafo_limpio)
+	print(lista_parrafos[0])
 
 
 #------------------aqui busca y lista 5 links de cada pregunta-------
 
-
-
-links=[]
-
 pregunta_google= input("ingresa tu pregunta: ")
 for i in search(pregunta_google, num_results=5, lang="es"):
-	
 	links.append(i)
 
 
